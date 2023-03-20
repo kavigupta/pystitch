89bb810cf4de5f3f762330899e32411d6e2b3283
Kavi Gupta <kavi@berkeley.edu>
2023-03-20 19:20:20 -0400

    update

--- pystitch/to_s.py
+++ pystitch/to_s.py
@@ -8,6 +8,8 @@ def field_is_body(t, f):
     assert isinstance(t, type)
     if t == ast.IfExp:
         return False # not body of statements
+    if t == ast.Lambda:
+        return False # not body of statements
     return f in {"body", "orelse", "finalbody"}
 
 
