72e9d269e573000992805d2e5c35c34511b8dd3d
Kavi Gupta <kavi@berkeley.edu>
2023-04-03 16:35:49 -0700

    update

---
+++ imperative_stitch/utils/recursion.py
@@ -0,0 +1,13 @@
+import sys
+
+class recursionlimit:
+    # https://stackoverflow.com/a/50120316/1549476
+    def __init__(self, limit):
+        self.limit = limit
+
+    def __enter__(self):
+        self.old_limit = sys.getrecursionlimit()
+        sys.setrecursionlimit(self.limit)
+
+    def __exit__(self, type, value, tb):
+        sys.setrecursionlimit(self.old_limit)
