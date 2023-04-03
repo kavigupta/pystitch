d00d5872bfafdf130e81cf5bd4e35a09a233f04c
Kavi Gupta <kavi@berkeley.edu>
2023-04-03 16:31:07 -0400

    fdix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="permacache",
+    name="imperative_stitch",
     version="3.6.2",
     author="Kavi Gupta, Matt Bowers",
     author_email="imperative-stitch@kavigupta.org",
