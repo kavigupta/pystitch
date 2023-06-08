4ac1fec69a1aff84da8b3af3418f70ba7a1f3c05
Kavi Gupta <kavi@berkeley.edu>
2023-06-07 23:06:32 -0400

    add more things

--- proofs/program.v
+++ proofs/program.v
@@ -24,7 +24,10 @@ Inductive prog :=
   (* add multi-assignment *)
   | prog_s : prog -> prog -> prog
   | prog_i : expr -> prog -> prog -> prog
-  | prog_w : expr -> prog -> prog -> prog
+  (* | prog_w : expr -> prog -> prog -> prog *)
+  | prog_b : prog
+  | prog_c : prog
+  | prog_r : expr -> prog
   (* add while/for/try/def *)
 .
 
@@ -42,20 +45,25 @@ Inductive exec_p : environment -> prog -> environment -> list effect -> continua
   | exec_p_a
     : forall env_1 env_2 effs v e,
       (exists x, exec_e env_1 e x effs /\ env_2 = cons (v, x) env_1) -> exec_p env_1 (prog_a v e) env_2 effs normal
-  | exec_p_s_normal
+  | exec_p_s_n
     : forall env_1 env_2 effs p_1 p_2 c,
       (exists env_inter eff_1 eff_2,
         exec_p env_1 p_1 env_inter eff_1 normal /\ exec_p env_inter p_2 env_2 eff_2 c /\ app eff_1 eff_2 = effs
       ) -> exec_p env_1 (prog_s p_1 p_2) env_2 effs c
-  (*| exec_p_s_abnormal
+  | exec_p_s_bcr
     : forall env_1 env_2 effs p_1 p_2 c,
       exec_p env_1 p_1 env_2 effs c /\ c <> normal
-       -> exec_p env_1 (prog_s p_1 p_2) env_2 effs c*)
+       -> exec_p env_1 (prog_s p_1 p_2) env_2 effs c
   | exec_p_i
     : forall env_1 env_2 effs cond p_1 p_2 c,
       (exists x eff_1 eff_2,
         exec_e env_1 cond x eff_1 /\ exec_p env_1 (if truthy x then p_1 else p_2) env_2 eff_2 c /\ app eff_1 eff_2 = effs
       ) -> exec_p env_1 (prog_i cond p_1 p_2) env_2 effs c
+  | exec_p_b : forall env, exec_p env prog_b env nil break
+  | exec_p_c : forall env, exec_p env prog_c env nil continue
+  | exec_p_r : forall env effs x e, 
+      exec_e env e x effs
+        -> exec_p env (prog_r e) env effs (returns x)
 .
 
 Hypothesis exec_e_deterministic : forall {env e x_a effs_a x_b effs_b},
@@ -74,7 +82,7 @@ Theorem exec_p_deterministic : forall {p env_1 env_2a effs_a env_2b effs_b c_a c
     end.
   all: unfold equal_env in *.
   all: subst.
-  4: destruct (truthy x2).
+  all: try (destruct (truthy x2)).
   all: repeat match goal with
      | [
       IH : forall _ _ _ _ _ _ _, exec_p _ ?p _ _ _ -> _,
@@ -84,8 +92,7 @@ Theorem exec_p_deterministic : forall {p env_1 env_2a effs_a env_2b effs_b c_a c
         destruct (IH _ _ _ _ _ _ _ HA HB) as [? [? ?]]; clear IH; subst
    end.
   all: try solve [subst; repeat split; auto].
+  all: try contradiction.
 Qed.
 
 End programs.
-
-Type prog.
