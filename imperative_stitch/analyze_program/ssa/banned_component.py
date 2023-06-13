03fa73843bd89381e8da219e2a230fc040b15868
Kavi Gupta <kavi@berkeley.edu>
2023-06-13 18:27:46 -0400

    update

--- imperative_stitch/analyze_program/ssa/banned_component.py
+++ imperative_stitch/analyze_program/ssa/banned_component.py
@@ -10,3 +10,8 @@ class BannedComponentVisitor(ast.NodeVisitor):
         raise BannedComponentError(
             "with statements cannot be used because of a limitation with python-graphs"
         )
+
+    def visit_ClassDef(self, node):
+        raise BannedComponentError(
+            "inner classes cannot be used because we do not support them yet"
+        )
