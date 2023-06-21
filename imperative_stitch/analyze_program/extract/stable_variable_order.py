c8d4c12b2acf61864b241f64e4bceb7c9d8c59a9
Kavi Gupta <kavi@berkeley.edu>
2023-06-21 02:21:37 -0400

    update canonicalization code

--- imperative_stitch/analyze_program/extract/stable_variable_order.py
+++ imperative_stitch/analyze_program/extract/stable_variable_order.py
@@ -2,29 +2,31 @@ import ast
 import ast_scope
 
 
-def compute_variable_order(func_def):
+def get_name_and_scope_each(func_def):
+    args = {x for x in ast.walk(func_def.args)}
     annotation = ast_scope.annotate(func_def)
-    nodes = [
-        x
-        for x in ast.walk(func_def)
-        if not isinstance(x, ast.arg)
-        and x in annotation
-        and isinstance(annotation[x], ast_scope.scope.FunctionScope)
-        and annotation[x].function_node == func_def
-    ]
-    if not nodes:
-        return []
-    to_name = annotation[nodes[0]].variables.node_to_symbol
-    nodes = [to_name[x] for x in nodes]
-    order = {}
-    for i, x in enumerate(nodes):
-        if x not in order:
-            order[x] = i
-    return order
+    nodes = [x for x in ast.walk(func_def) if x in annotation]
+    node_to_name_and_scope = {}
+    name_and_scope_ordering = {}
+    for i, node in enumerate(nodes):
+        if isinstance(node, ast.arg):
+            continue
+        scope = annotation[node]
+        name = scope.variables.node_to_symbol[node]
+        node_to_name_and_scope[node] = (name, scope)
+        if node not in args:
+            if (name, scope) not in name_and_scope_ordering:
+                name_and_scope_ordering[(name, scope)] = i
+    return (
+        node_to_name_and_scope,
+        name_and_scope_ordering,
+        annotation.function_scope_for(func_def),
+    )
 
 
 def canonicalize_variable_order(func_def, input_variables, output_variables):
-    variable_order = compute_variable_order(func_def)
-    input_variables = sorted(input_variables, key=lambda x: variable_order[x])
-    output_variables = sorted(output_variables, key=lambda x: variable_order[x])
+    _, name_and_scope_ordering, scope = get_name_and_scope_each(func_def)
+    var_order = lambda x: name_and_scope_ordering[(x, scope)]
+    input_variables = sorted(input_variables, key=var_order)
+    output_variables = sorted(output_variables, key=var_order)
     return input_variables, output_variables
