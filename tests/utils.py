42ee467d59a82037bc14766591e5e0b6a8c44154
Kavi Gupta <kavi@berkeley.edu>
2023-07-12 19:57:31 -0400

    update small set of runnable code examples

--- tests/utils.py
+++ tests/utils.py
@@ -15,3 +15,10 @@ def small_set_examples():
     with open("data/small_set.json") as f:
         contents = json.load(f)
     return contents
+
+
+@lru_cache(None)
+def small_set_runnable_code_examples():
+    with open("data/small_set_runnable_code.json") as f:
+        contents = json.load(f)
+    return contents
