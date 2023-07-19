b86228b5896897bbc9795f6968449231557085f0
Kavi Gupta <kavi@berkeley.edu>
2023-07-19 16:01:40 -0400

    ban more components

--- imperative_stitch/analyze_program/ssa/banned_component.py
+++ imperative_stitch/analyze_program/ssa/banned_component.py
@@ -1,58 +1,43 @@
 import ast
+from dataclasses import dataclass
 
 
+@dataclass(eq=True)
 class BannedComponentError(Exception):
-    def __eq__(self, other):
-        return isinstance(other, BannedComponentError) and self.args == other.args
+    component_type: str
+    at_fault: ast.AST
 
 
 class BannedComponentVisitor(ast.NodeVisitor):
     def visit_With(self, node):
-        raise BannedComponentError(
-            "with statements cannot be used because of a limitation with python-graphs"
-        )
+        raise BannedComponentError("with", "python-graphs")
 
     def visit_ClassDef(self, node):
-        raise BannedComponentError(
-            "inner classes cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("classes", "us")
 
     def visit_Nonlocal(self, node):
-        raise BannedComponentError(
-            "nonlocal statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("nonlocal", "us")
 
     def visit_NamedExpr(self, node):
-        raise BannedComponentError(
-            "walrus statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("walrus operator", "us")
 
     def visit_Yield(self, node):
-        raise BannedComponentError(
-            "yield statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("yield", "us")
 
     def visit_YieldFrom(self, node):
-        raise BannedComponentError(
-            "yield from statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("yield from", "us")
 
     def visit_AsyncFor(self, node):
-        raise BannedComponentError(
-            "async for statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("async for", "us")
 
     def visit_AsyncWith(self, node):
-        raise BannedComponentError(
-            "async with statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("async with", "us")
 
     def visit_AsyncFunctionDef(self, node):
-        raise BannedComponentError(
-            "async functions cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("async functions", "us")
 
     def visit_Await(self, node):
-        raise BannedComponentError(
-            "await statements cannot be used because we do not support them yet"
-        )
+        raise BannedComponentError("await", "us")
+
+    def visit_Global(self, node):
+        raise BannedComponentError("global", "us")
