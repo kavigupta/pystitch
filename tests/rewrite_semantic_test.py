2d1b818b24c13863ec342ec00146b2bf443f07d9
Kavi Gupta <kavi@berkeley.edu>
2023-07-19 15:53:14 -0400

    avoid recursion errors in testing

--- tests/rewrite_semantic_test.py
+++ tests/rewrite_semantic_test.py
@@ -29,16 +29,33 @@ class RewriteSemanticsTest(GenericRewriteRealisticTest):
             print(new_code)
             for input, output in zip(example["inputs"], example["outputs"]):
                 print(repr(input))
-                py_out = run_python(new_code, input)
-                if py_out is None:
-                    run_python.function(new_code, input)
-                    self.fail("error")
+                py_out = self.run_and_check_for_errors(new_code, input)
                 py_out = normalize_output(py_out)
                 output = normalize_output(output)
                 if py_out != output:
                     if not self.check_deterministic(code_original, input):
                         continue
                 self.assertEqual(py_out, output)
+
+    def run_and_check_for_errors(self, code, input):
+        py_out = run_python(code, input)
+        if py_out is not None:
+            return py_out
+        code = self.add_header(code)
+        py_out = run_python(code, input)
+        if py_out is not None:
+            return py_out
+        run_python.function(code, input)
+        self.fail("error")
+
+    def add_header(self, code):
+        recursion_header = ast.parse(
+            "import sys; sys.setrecursionlimit(10 ** 8); sys.setrecursionlimit = lambda x: None"
+        )
+        code = ast.parse(code)
+        code.body = recursion_header.body + code.body
+        code = ast.fix_missing_locations(code)
+        return ast.unparse(code)
 
     def check_deterministic(self, code, input):
         outputs = []
