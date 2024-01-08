4320f5764c49b3e541e0770d60df6169c1de2d30
Kavi Gupta <kavi@berkeley.edu>
2024-01-07 20:24:11 -0800

    simplify conversion logic by placing more of it inside ParsedAST as a class (#7)
    * simplify conversion logic by placing more of it inside ParsedAST as a class
    
    * update import

--- imperative_stitch/parser/convert.py
+++ imperative_stitch/parser/convert.py
@@ -1,30 +1,15 @@
-import ast
-
-from s_expression_parser import Renderer
-
 from imperative_stitch.parser.parsed_ast import ParsedAST
-from imperative_stitch.utils.recursion import limit_to_size
 
 
 def python_to_s_exp(code, renderer_kwargs=None):
     """
     Converts python code to an s-expression.
     """
-    if renderer_kwargs is None:
-        renderer_kwargs = {}
-    with limit_to_size(code):
-        code = ParsedAST.parse_python_code(code)
-        code = code.to_pair_s_exp()
-        code = Renderer(**renderer_kwargs, nil_as_word=True).render(code)
-        return code
+    return ParsedAST.parse_python_code(code).to_s_exp(renderer_kwargs)
 
 
 def s_exp_to_python(code):
     """
     Converts an s expression to python code.
     """
-    with limit_to_size(code):
-        code = ParsedAST.parse_s_expression(code)
-        code = code.to_python_ast()
-        code = ast.unparse(code)
-        return code
+    return ParsedAST.parse_s_expression(code).to_python()
