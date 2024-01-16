e383fc731b5bb1072fc3eb5ec1c6a76a3f8354ab
Kavi Gupta <kavi@berkeley.edu>
2024-01-16 18:37:21 -0500

    update stitch output set to not skip 171

---
+++ imperative_stitch/data/stitch_output_set.py
@@ -0,0 +1,66 @@
+import json
+import os
+
+import fire
+from datasets import load_dataset
+from permacache import permacache, stable_hash
+import tqdm
+
+from imperative_stitch.parser import python_to_s_exp
+
+from imperative_stitch.data.compression_testing_code import compression_testing_code
+from imperative_stitch.compress.julia_stitch import run_julia_stitch
+
+
+@permacache(
+    "imperative_stitch/data/stitch_output_set/run_stitch_cached",
+    key_function=dict(c=stable_hash),
+)
+def run_stitch_cached(c):
+    return run_julia_stitch(
+        c,
+        stitch_jl_dir="../Stitch.jl/",
+        iters=1,
+        application_utility_symvar=-0.5,
+        max_arity=4,
+        quiet=False,
+    )
+
+
+@permacache(
+    "imperative_stitch/data/stitch_output_set/stitch_output_set_3",
+)
+def stitch_output_set(amount):
+    sets = compression_testing_code(amount * 10)
+    sets = [x[:10] for x in sets]
+
+    results = []
+
+    pbar = tqdm.tqdm(total=amount)
+
+    for s in sets:
+        if len(json.dumps(s)) > 5000:
+            continue
+        c = [python_to_s_exp(code) for code in s]
+
+        abstractions, rewritten = run_stitch_cached(c)
+
+        result = dict(
+            code=c,
+            abstractions=abstractions,
+            rewritten=rewritten,
+        )
+
+        results.append(result)
+        pbar.update(1)
+
+        if len(results) >= amount:
+            pbar.close()
+            break
+
+    return results
+
+
+if __name__ == "__main__":
+    with open("data/stitch_output_set.json", "w") as f:
+        json.dump(stitch_output_set(100), f, indent=2)
