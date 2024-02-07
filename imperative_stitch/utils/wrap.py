a6251159d4169ae0353cf8d344774b67ceb63f57
Kavi Gupta <kavi@berkeley.edu>
2024-02-06 23:09:24 -0500

    unwrap (#32)

--- imperative_stitch/utils/wrap.py
+++ imperative_stitch/utils/wrap.py
@@ -43,3 +43,42 @@ def wrap_ast(code, fn_name="_main"):
 
 def wrap(code, fn_name="_main"):
     return ast.unparse(ast.fix_missing_locations(wrap_ast(ast.parse(code), fn_name)))
+
+
+def clean_for_unwrap(body):
+    if body and isinstance(body[-1], ast.Pass):
+        body = body[:-1]
+    if body and isinstance(body[-1], ast.Return):
+        body[-1] = ast.Expr(value=body[-1].value)
+    return body
+
+
+def unwrap_ast(code):
+    """
+    Unwrap the code.
+    """
+    assert isinstance(code, ast.Module)
+    body = code.body
+    assert isinstance(body, list)
+    assert len(body) >= 2
+    imports = []
+    for node in body:
+        if isinstance(node, ast.Import):
+            imports.append(node)
+        elif isinstance(node, ast.ImportFrom):
+            imports.append(node)
+        else:
+            break
+    body = body[len(imports) :]
+    assert len(body) == 2
+    assert isinstance(body[0], ast.FunctionDef)
+    output = imports + clean_for_unwrap(body[0].body)
+    assert ast.unparse(body[1]) == f"{body[0].name}()", (
+        ast.unparse(body[1]),
+        body[0].name,
+    )
+    return ast.Module(body=output, type_ignores=[])
+
+
+def unwrap(code):
+    return ast.unparse(unwrap_ast(ast.parse(code)))
---
+++ tests/utility_test.py
@@ -0,0 +1,92 @@
+import unittest
+from textwrap import dedent
+
+from imperative_stitch.utils.wrap import unwrap, wrap
+from tests.utils import canonicalize
+
+
+class WrapTest(unittest.TestCase):
+    def assertSameCode(self, expected, actual):
+        self.assertEqual(canonicalize(expected), canonicalize(actual))
+
+    def test_basic_wrap(self):
+        code = "x = 3"
+        expected = dedent(
+            """
+            def _main():
+                x = 3
+            _main()
+            """
+        )
+        self.assertSameCode(wrap(code), expected)
+
+    def test_wrap_with_imports(self):
+        code = dedent(
+            """
+            import os
+            from math import sqrt
+            x = 3
+            """
+        )
+        expected = dedent(
+            """
+            import os
+            from math import sqrt
+            def _main():
+                x = 3
+            _main()
+            """
+        )
+        self.assertSameCode(wrap(code), expected)
+
+    def test_unwrap_basic(self):
+        code = dedent(
+            """
+            def _main():
+                x = 3
+            _main()
+            """
+        )
+        expected = "x = 3"
+        self.assertSameCode(unwrap(code), expected)
+
+    def test_unwrap_with_imports(self):
+        code = dedent(
+            """
+            import os
+            from math import sqrt
+            def _main():
+                x = 3
+            _main()
+            """
+        )
+        expected = dedent(
+            """
+            import os
+            from math import sqrt
+            x = 3
+            """
+        )
+        self.assertSameCode(unwrap(code), expected)
+
+    def test_unwrap_empty(self):
+        code = dedent(
+            """
+            def _main():
+                pass
+            _main()
+            """
+        )
+        expected = ""
+        self.assertSameCode(unwrap(code), expected)
+
+    def test_unwrap_with_return(self):
+        code = dedent(
+            """
+            def _main():
+                return 3
+            _main()
+            """
+        )
+        expected = "3"
+        self.assertSameCode(unwrap(code), expected)
