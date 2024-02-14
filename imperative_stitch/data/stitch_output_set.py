74cd7bf7514857ecc05365c39e58d628e6380ea0
Kavi Gupta <kavi@berkeley.edu>
2024-02-13 23:25:42 -0500

    bump to handle newest julia stitch

--- imperative_stitch/data/stitch_output_set.py
+++ imperative_stitch/data/stitch_output_set.py
@@ -10,7 +10,7 @@ from imperative_stitch.parser import python_to_s_exp
 
 
 @permacache(
-    "imperative_stitch/data/stitch_output_set/run_stitch_cached_8",
+    "imperative_stitch/data/stitch_output_set/run_stitch_cached_9",
     key_function=dict(c=stable_hash),
 )
 def run_stitch_cached(c):
@@ -28,7 +28,7 @@ def run_stitch_cached(c):
 
 
 @permacache(
-    "imperative_stitch/data/stitch_output_set/stitch_output_set_12",
+    "imperative_stitch/data/stitch_output_set/stitch_output_set_13",
 )
 def stitch_output_set(amount):
     sets = compression_testing_code(amount * 10)
