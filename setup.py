97c763f78686892949c25453b53a222a99031f3d
Kavi Gupta <kavi@berkeley.edu>
2024-03-18 17:30:01 -0400

    bump neurosym version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imperative_stitch",
-    version="3.6.13",
+    version="3.6.14",
     author="Kavi Gupta, Matt Bowers",
     author_email="imperative-stitch@kavigupta.org",
     description="Imperative version of stitch.",
@@ -25,6 +25,6 @@ setuptools.setup(
         "frozendict==2.3.8",
         # my fork of python-graphs, update this to the latest commit hash
         "python-graphs @ https://github.com/kavigupta/python-graphs/archive/693b2bc5e65a0f930617d75c3fb0c1750d35a622.zip",
-        "neurosym==0.0.24",
+        "neurosym==0.0.25",
     ],
 )
