fdd6924127973faae8d2c4092ebd136687e618e2
Kavi Gupta <kavi@berkeley.edu>
2024-03-19 02:51:11 -0400

    update

--- tests/dsl_tests/def_use_mask_test.py
+++ tests/dsl_tests/def_use_mask_test.py
@@ -325,8 +325,8 @@ class EnumerateFittedDslTest(unittest.TestCase):
 
     @expand_with_slow_tests(1000, -1)
     def test_realistic(self, i):
-        if i == 22 or i == 31 or i == 41:
-            # forward declaration of input for 22/41, n for 31
+        if i in {22, 31, 41, 57}:
+            # forward declaration of input for 22/41, n for 31/57
             return
         example = small_set_runnable_code_examples()[i]["solution"]
         print(example)
