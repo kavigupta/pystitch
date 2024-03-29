64194ab40ec53638c63e01442184627367badfe8
Kavi Gupta <kavi@berkeley.edu>
2024-03-28 22:43:32 -0400

    update

--- imperative_stitch/utils/def_use_mask/abstraction_handler.py
+++ imperative_stitch/utils/def_use_mask/abstraction_handler.py
@@ -6,6 +6,31 @@ from .handler import Handler, default_handler
 
 
 class AbstractionHandler(Handler):
+    """
+    Handler for an abstraction node. This effectively runs through
+        the body of the abstraction, pausing at each abstraction variable
+        to wait for the next node in the tree to be processed.
+
+    E.g., in an abstraction node (fn_1 (Constant i2 1) &x:0), where fn_1 has body
+            (Assign (Name %1 Store) #0)
+        the handler would traverse Assign then Name, then pause, awaiting the
+            next argument to be passed in. It would then get &x:0 passed in [since
+            abstractions are processed in a custom order related to the ordering
+            of the arguments' appearances in the body], and continue traversing,
+            substituting &x:0 for the passed in node.
+
+    The way this is accomplished is via the _body_handler coroutine, which yields
+        a boolean value indicating whether the current argument is defining a variable,
+        and is sent the next node in the tree to process. This is done via a coroutine
+        because that is the simplest way to have a recursive function that can pause.
+
+    The coroutine iterates on a copy of the def-use mask, which is important because
+        the original mask used to create the AbstractionHandler will be modified
+        as the arguments to the abstraction are processed. The copy is created with
+        a single handler, which is a default handler for the body.
+
+    """
+
     def __init__(self, mask, defined_production_idxs, config, head_symbol):
         super().__init__(mask, defined_production_idxs, config)
         head_symbol = "~".join(head_symbol.split("~")[:-1])
@@ -25,6 +50,10 @@ class AbstractionHandler(Handler):
         self._iterate_body(None)
 
     def on_child_enter(self, position: int, symbol: int) -> "Handler":
+        """
+        Make sure to collect the children of the abstraction, so it can
+            be iterated once the abstraction is fully processed.
+        """
         return CollectingHandler(
             symbol,
             super().on_child_enter(position, symbol),
@@ -39,6 +68,7 @@ class AbstractionHandler(Handler):
 
     def body_traversal_coroutine(self, node, position):
         if VARIABLE_REGEX.match(node.symbol):
+            # If the node is a variable, check if it is one that has already been processed
             name = node.symbol
             if name in self._variables_to_reuse:
                 node = self._variables_to_reuse[name]
@@ -71,6 +101,9 @@ class AbstractionHandler(Handler):
 
 
 class CollectingHandler(Handler):
+    """
+    Wrapper around another handler that collects the node as it is being created.
+    """
     def __init__(self, sym, underlying_handler):
         super().__init__(
             underlying_handler.mask,
