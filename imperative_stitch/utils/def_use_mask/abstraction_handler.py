ca2f9c88a427c244c6b7e985b24f4aad7172a830
Kavi Gupta <kavi@berkeley.edu>
2024-04-03 13:46:31 -0400

    fix exit likelihood

--- imperative_stitch/utils/def_use_mask/abstraction_handler.py
+++ imperative_stitch/utils/def_use_mask/abstraction_handler.py
@@ -126,7 +126,7 @@ class CollectingHandler(Handler):
         sym, arity = self.mask.tree_dist.symbols[self.sym]
         assert (
             len(self.children) == arity
-        ), f"Expected {arity} children, got {len(self.children)}"
+        ), f"{sym} expected {arity} children, got {len(self.children)}"
         return ns.SExpression(
             sym, [self.children[i].node for i in range(len(self.children))]
         )
