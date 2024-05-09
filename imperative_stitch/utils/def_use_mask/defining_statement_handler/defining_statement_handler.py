4ff51f9537a4153157380929c5aac2a38833f524
Kavi Gupta <kavi@berkeley.edu>
2024-05-09 17:07:28 -0400

    Revert "ensure defined_symbols has no duplications"
    This reverts commit d8d8d0612dee4f52b5f376eced88affd51c47d40.

--- imperative_stitch/utils/def_use_mask/defining_statement_handler/defining_statement_handler.py
+++ imperative_stitch/utils/def_use_mask/defining_statement_handler/defining_statement_handler.py
@@ -28,10 +28,7 @@ class DefiningStatementHandler(ConstructHandler):
         if position in self._targeted_positions:
             self.defined_symbols += child.defined_symbols
         if position == self.child_fields[self.define_symbols_on_exit]:
-            current = set(self.defined_production_idxs)
-            self.defined_production_idxs += [
-                x for x in self.defined_symbols if x not in current
-            ]
+            self.defined_production_idxs += self.defined_symbols
         super().on_child_exit(position, symbol, child)
 
     def is_defining(self, position: int) -> bool:
