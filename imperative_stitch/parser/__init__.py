c0058ab9d4a79f8a6344131ca6fa41b31fd387cb
Kavi Gupta <kavi@berkeley.edu>
2024-05-28 19:25:18 -0400

    delete imports

--- imperative_stitch/parser/__init__.py
+++ imperative_stitch/parser/__init__.py
@@ -6,5 +6,4 @@ There are three representations of a python program.
     3. s-expressions for stitch. E.g., "(Assign (list (Name &x:0 Store)) (Constant i2 None) None)"
 """
 
-from .converter import python_to_s_exp, s_exp_to_python
 from .python_ast import PythonAST
