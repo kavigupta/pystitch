ec0a51e3ffcbcbf93662a07880b37af237c5bd16
Kavi Gupta <kavig@mit.edu>
2024-06-04 17:41:19 -0400

    Move tests to for the def-use mask to neurosym (#183)

--- tests/dsl_tests/likelihood_fitted_dsl_test.py
+++ tests/dsl_tests/likelihood_fitted_dsl_test.py
@@ -29,52 +29,6 @@ class TestLikelihoodFittedDSL(unittest.TestCase):
         print(results)
         return like, results
 
-    def test_likelihood(self):
-        like, results = self.compute_likelihood(["x = 2", "y = 3", "y = 4"], "y = 4")
-        self.assertAlmostEqual(like, Fraction(2, 9))
-        self.assertEqual(
-            results,
-            [
-                ("(const-&y:0~Name)", Fraction(2, 3)),
-                ("(const-i4~Const)", Fraction(1, 3)),
-            ],
-        )
-
-    def test_likelihood_def_use_check(self):
-        like, results = self.compute_likelihood(
-            ["x = 2; y = x", "y = 2; x = y"], "x = 2; y = x"
-        )
-        self.assertAlmostEqual(like, Fraction(1, 8))
-        self.assertEqual(
-            results,
-            [
-                ("(const-&x:0~Name)", Fraction(1, 2)),
-                ("(const-&y:0~Name)", Fraction(1, 2)),
-                ("(Name~E (const-&x:0~Name) (Load~Ctx))", Fraction(1, 2)),
-            ],
-        )
-
-    def test_likelihood_zero(self):
-        like, results = self.compute_likelihood(
-            ["y = x + 2", "y = 2 + 3", "y = 4"], "y = 2 + x"
-        )
-        self.assertAlmostEqual(like, Fraction(0))
-        self.assertEqual(
-            results,
-            [
-                (
-                    "(BinOp~E (Constant~E (const-i2~Const) (const-None~ConstKind)) (Add~O) (Name~E (const-g_x~Name) (Load~Ctx)))",
-                    Fraction(2, 3),
-                ),
-                (
-                    "(Constant~E (const-i2~Const) (const-None~ConstKind))",
-                    Fraction(1, 2),
-                ),
-                ("(const-i2~Const)", Fraction(1, 2)),
-                ("(Name~E (const-g_x~Name) (Load~Ctx))", Fraction(0, 1)),
-            ],
-        )
-
     def test_likelihood_with_abstractions(self):
         # test from annie
         test_programs = [
