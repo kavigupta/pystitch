fdb355460052f0aeff3ee598ba137bb32a67de4d
Kavi Gupta <kavi@berkeley.edu>
2024-09-25 13:12:00 -0700

    update compression hyperparameter search to parameterize over the hyperparameter generation

--- imperative_stitch/experiments/compression_hyperparameter_search/run_experiment.py
+++ imperative_stitch/experiments/compression_hyperparameter_search/run_experiment.py
@@ -1,34 +1,39 @@
 from tqdm.contrib.concurrent import process_map
 
 from .datasets import datasets
-from .hyperparameters import sample_hyperparameters
 from .run_stitch import run_stitch_with_hyperparameters
 
 
-def run_experiment_up_to_seed(num_seeds, *, skip_missing):
+def run_experiment_up_to_seed(num_seeds, hypers_for_seed, *, skip_missing):
     data = datasets()
 
-    arguments = [(k, seed, skip_missing) for seed in range(num_seeds) for k in data]
+    arguments = [
+        (k, seed, hypers_for_seed(seed), skip_missing)
+        for seed in range(num_seeds)
+        for k in data
+    ]
     results_flat = process_map(run_experiment, arguments, max_workers=16)
-    results_flat = dict(zip(arguments, results_flat))
+    results_flat = {
+        (k, seed): result for (k, seed, _, _), result in zip(arguments, results_flat)
+    }
 
     results = []
     for seed in range(num_seeds):
         result = {}
         for k in data:
-            result[k] = results_flat[k, seed, skip_missing]
+            result[k] = results_flat[k, seed]
         results.append(result)
     return results
 
 
-def run_experiment(k_seed_skip_missing):
-    k, seed, skip_missing = k_seed_skip_missing
+def run_experiment(k_seed_hypers_skip_missing):
+    k, seed, hypers, skip_missing = k_seed_hypers_skip_missing
     data = datasets()
     kwargs = dict(
         dataset=[v for _, v in sorted(data[k].items())],
         stitch_jl_dir="../Stitch.jl/",
         iters=10,
-        **sample_hyperparameters(seed),
+        **hypers,
         print_before_after=f"{k} with seed {seed}",
     )
     if skip_missing and not run_stitch_with_hyperparameters.cache_contains(**kwargs):
