f15332623aaf9ae6261736aa02ab75f4ae63ecb4
Kavi Gupta <kavi@berkeley.edu>
2024-10-03 09:55:28 -0700

    create extract_code function, which is useful for analyses

--- imperative_stitch/experiments/compression_hyperparameter_search/analysis.py
+++ imperative_stitch/experiments/compression_hyperparameter_search/analysis.py
@@ -21,19 +21,28 @@ def print_out_abstractions(seed, result):
     for k in result:
         if result[k] is None:
             continue
-        (_, abstrs, rewr), _ = result[k]
-        abstrs = [Abstraction.of(name=f"fn_{i}", **x) for i, x in enumerate(abstrs, 1)]
-        abstrs_d = {abstr.name: abstr for abstr in abstrs}
-        if not abstrs:
+        abstr_names, codes, rewr = extract_code(result[k])
+        if not abstr_names:
             continue
         print("*" * 80)
         print(seed, k)
-        for abstr in abstrs:
+        for abstr_name, code in zip(abstr_names, codes):
             print(" " * 10, "*" * 40)
-            counts = [x.count(abstr.name) for x in rewr]
-            print(" " * 10, abstr.name, "::", sum(counts), [x for x in counts if x])
-            code = abstraction_calls_to_stubs(
-                abstr.body_with_variable_names(), abstrs_d
-            ).to_python()
+            counts = [x.count(abstr_name) for x in rewr]
+            print(" " * 10, abstr_name, "::", sum(counts), [x for x in counts if x])
             code = code.replace("\n", "\n" + " " * 11)
             print(" " * 10, code)
+
+
+def extract_code(result):
+    (_, abstrs, rewr), _ = result
+    abstrs = [Abstraction.of(name=f"fn_{i}", **x) for i, x in enumerate(abstrs, 1)]
+    abstrs_d = {abstr.name: abstr for abstr in abstrs}
+    codes = [
+        abstraction_calls_to_stubs(
+            abstr.body_with_variable_names(), abstrs_d
+        ).to_python()
+        for abstr in abstrs
+    ]
+    abstr_names = [abstr.name for abstr in abstrs]
+    return abstr_names, codes, rewr
